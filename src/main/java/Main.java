5464ff98ff85dddcc7c709cf6341901303e59037
CiprianNGR <cipriannegrut@yahoo.com>
2022-02-24 14:58:24 +0200

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+///For update GirLab
+///1. git commit -a -m "initial commit"
+///2. git push -uf origin main
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+
+}
---
+++ src/main/java/Monomial.java
@@ -0,0 +1,11 @@
+public class Monomial {
+
+    private double real;
+    private double imag;
+
+    public Monomial(double real, double imag){
+        this.real = real;
+        this.imag = imag;
+    }
+
+}
